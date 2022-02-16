c8517ac17ea4aebef04e9765bfa9d44ec994e762
Gaston Perez <gastonperez-97@hotmail.com>
2022-02-16 15:48:46 -0300

    added default gift img, gift form actions and modified form validations. edit gift implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import { useEffect, useState } from "react";
+import defaultGiftImg from "./assets/img/gift-default.png";
+import GiftForm, { GIFT_FORM_ACTIONS } from "./components/GiftForm/GiftForm";
 
-import GiftForm from "./components/GiftForm/GiftForm";
+import { IoIosTrash } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { BiDuplicate } from "react-icons/bi";
 
 import './App.css';
 
 const App = () => {
 	const [gifts, setGifts] = useState(JSON.parse(localStorage.getItem("gifts")) || []);
+	const [selectedGiftData, setSelectedGiftData] = useState(null);
 	const [showGiftForm, setShowGiftForm] = useState(false);
+	const [giftFormAction, setGiftFormAction] = useState("");
 
 	useEffect(() => {
 		localStorage.setItem("gifts", JSON.stringify(gifts));
 	}, [gifts]);
 
-	const closeGiftForm = () => setShowGiftForm(false);
+	const getGift = giftId => gifts.find(gift => gift.id === giftId);
+
+	const closeGiftForm = () => {
+		setShowGiftForm(false);
+		setSelectedGiftData(null);
+	}
+
+	const addNewGift = () => {
+		setShowGiftForm(true);
+		setGiftFormAction(GIFT_FORM_ACTIONS.ADD);
+	}
 
 	const addGift = gift => {
 		setGifts(prevGifts => [...prevGifts, gift]);
@@ -23,19 +39,55 @@ const App = () => {
 
 	const deleteAllGifts = () => setGifts([]);
 
+	const editGift = gift => {
+		setGifts(prevGifts => {
+			return prevGifts.map(prevGift => {
+				return prevGift.id === gift.id ? gift : prevGift;
+			});
+		});
+
+		closeGiftForm();
+	}
+
+	const handleEditGift = giftId => {
+		setShowGiftForm(true);
+		setGiftFormAction(GIFT_FORM_ACTIONS.EDIT);
+		setSelectedGiftData(getGift(giftId));
+	}
+
 	const giftElements = gifts.map(gift => {
 		return (
 			<li key={ gift.id }>
-				<div>
-					<img src={ gift.imageUrl } alt="Imagen del regalo" />
-					<p>{ gift.title } x{ gift.qty }</p>
+				<div className="gift-info-container">
+					<img
+						src={ gift.imageUrl }
+						alt="Imagen del regalo"
+						onError={ event => {
+							event.target.src = defaultGiftImg;
+						}}
+					/>
+					
+					<div className="gift-text">
+						<p>{ gift.title } x{ gift.qty }</p>
+						<small>{ gift.receiver }</small>
+					</div>
 				</div>
-				
-				<button
-					className="btn-red btn-gift"
-					onClick={ () => deleteGift(gift.id) }
-				>X
-				</button>
+
+				<div className="gift-btn-container">
+					<button
+						className="btn-red btn-gift"
+						onClick={ () => handleEditGift(gift.id) }
+					>
+						<FiEdit />
+					</button>
+
+					<button
+						className="btn-red btn-gift"
+						onClick={ () => deleteGift(gift.id) }
+					>
+						<IoIosTrash />
+					</button>
+				</div>
 			</li>
 		);
 	});
@@ -48,14 +100,17 @@ const App = () => {
 
 					<button
 						className="btn-red btn-add-gift"
-						onClick={ () => setShowGiftForm(true) }
+						onClick={ addNewGift }
 					>Agregar regalo
 					</button>
 
 					{ showGiftForm &&
 						<GiftForm
+							giftFormAction={ giftFormAction }
 							addGift={ addGift }
-							closeGiftForm={ closeGiftForm }	
+							editGift={ editGift }
+							selectedGiftData={ selectedGiftData }
+							closeGiftForm={ closeGiftForm }
 						/>
 					}
 
--- src/components/GiftForm/GiftForm.js
+++ src/components/GiftForm/GiftForm.js
@@ -5,72 +5,139 @@ import "./GiftForm.css";
 
 import { BiErrorCircle } from "react-icons/bi";
 
+const GIFT_FORM_ACTIONS = {
+	ADD: "Agregar",
+	EDIT: "Editar",
+	DUPLICATE: "Duplicar"
+}
+
 const GiftForm = props => {
 	const newGift = () => {
 		return {
 			id: nanoid(),
 			title: "",
 			qty: "",
-			imageUrl: ""
+			imageUrl: "",
+			receiver: ""
 		}
 	}
 
-	const [giftFormData, setGiftFormData] = useState(newGift());
-	const [giftFormError, setGiftFormError] = useState("");
+	const [giftFormData, setGiftFormData] = useState(props.selectedGiftData !== null ? props.selectedGiftData : newGift());
+	const [showGiftFormErrors, setShowGiftFormErrors] = useState(false);
 
 	const handleFormChange = event => {
 		const {name, value} = event.target;
-
 		setGiftFormData(prevFormData => ({...prevFormData, [name]: value}));
 	}
 
 	const handleSubmit = () => {
-		if (!giftFormData.title || giftFormData.qty <= 0 || !giftFormData.imageUrl) {
-			setGiftFormError("Completa los campos para agregar el regalo a la lista.");
+		if (!giftFormData.title
+			|| giftFormData.qty <= 0
+			|| !giftFormData.imageUrl
+			|| !giftFormData.receiver) {
+				
+			setShowGiftFormErrors(true);
 		} else {
-			props.addGift(giftFormData);
+			switch (props.giftFormAction) {
+				case GIFT_FORM_ACTIONS.ADD:
+					props.addGift(giftFormData);
+					break;
+
+				case GIFT_FORM_ACTIONS.EDIT:
+					props.editGift(giftFormData);
+					break;
+			
+				default:
+					break;
+			}
+
 			setGiftFormData(newGift());
-			setGiftFormError("");
+			setShowGiftFormErrors(false);
 		}
-	} 
+	}
 
 	return (
 		<section className="form-main-container">
 			<div className="form-container">
-				<h2>Agregar regalo:</h2>
+				<h2>{ props.giftFormAction } regalo:</h2>
 
-				<input
-					type="text"
-					name="title"
-					placeholder="&iquest;Qu&eacute; vas a regalar?"
-					value={ giftFormData.title }
-					onChange={ handleFormChange }
-				/>
-
-				<div className="form-input-row">
+				<div>
 					<input
 						type="text"
-						name="imageUrl"
-						placeholder="https://image..."
-						value={ giftFormData.imageUrl }
+						name="title"
+						placeholder="&iquest;Qu&eacute; vas a regalar?"
+						value={ giftFormData.title }
 						onChange={ handleFormChange }
 					/>
 
+					{ (showGiftFormErrors && !giftFormData.title) &&  
+						<div className="gift-form-error">
+							<BiErrorCircle />
+							<small>Completa este campo</small>
+						</div>
+					}
+				</div>
+
+				<div>
 					<input
-						type="number"
-						min={0}
-						name="qty"
-						placeholder="Cantidad"
-						value={ giftFormData.qty }
+						type="text"
+						name="receiver"
+						placeholder="&iquest;A qui&eacute;n se lo vas a regalar?"
+						value={ giftFormData.receiver }
 						onChange={ handleFormChange }
 					/>
+
+					{ (showGiftFormErrors && !giftFormData.receiver) &&  
+						<div className="gift-form-error">
+							<BiErrorCircle />
+							<small>Completa este campo</small>
+						</div>
+					}
+				</div>
+
+
+				<div className="form-input-row">
+					<div className="form-input-img">
+						<input
+							type="text"
+							name="imageUrl"
+							placeholder="https://imagen..."
+							value={ giftFormData.imageUrl }
+							onChange={ handleFormChange }
+						/>
+
+						{ (showGiftFormErrors && !giftFormData.imageUrl) &&  
+							<div className="gift-form-error">
+								<BiErrorCircle />
+								<small>Agrega una imagen</small>
+							</div>
+						}
+					</div>
+
+					<div className="form-input-qty">
+						<input
+							type="number"
+							min={0}
+							name="qty"
+							placeholder="Cantidad"
+							value={ giftFormData.qty }
+							onChange={ handleFormChange }
+						/>
+
+						{ (showGiftFormErrors && !giftFormData.qty) &&  
+							<div className="gift-form-error">
+								<BiErrorCircle />
+								<small>Agrega la cantidad</small>
+							</div>
+						}
+					</div>
 				</div>
 
 				<div className="form-btn-container">
 					<button
 						onClick={ handleSubmit }
 						className="btn-red"
-					>Agregar
+					>{ props.giftFormAction }
 					</button>
 
 					<button
@@ -80,15 +147,8 @@ const GiftForm = props => {
 					</button>
 				</div>
 			</div>
-
-			{ giftFormError &&
-				<div className="gift-form-error">
-					<BiErrorCircle />
-					<small>{ giftFormError }</small>
-				</div>
-			}
 		</section>
 	);
 }
 
-export default GiftForm;
+export { GiftForm as default, GIFT_FORM_ACTIONS };
